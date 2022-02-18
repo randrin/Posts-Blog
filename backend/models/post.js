8f1bd648c06627d888e8464835d9a3ff46a5c3d4
Randrin Nzeukang <nrandrin@eservicios.indracompany.com>
2022-02-18 23:22:20 +0100

    Update model

--- backend/models/post.js
+++ backend/models/post.js
@@ -1,10 +1,19 @@
 const mongoose = require("mongoose");
 
-const postSchema = mongoose.Schema({
-  title: { type: String, required: true },
-  content: { type: String, required: true },
-  imagePath: { type: String, required: true },
-  creator: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true }
-});
+const postSchema = mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    content: { type: String, required: true },
+    imagePath: { type: String, required: true },
+    creator: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = mongoose.model("Post", postSchema);
--- backend/models/user.js
+++ backend/models/user.js
@@ -1,10 +1,15 @@
 const mongoose = require("mongoose");
 const uniqueValidator = require("mongoose-unique-validator");
 
-const userSchema = mongoose.Schema({
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true }
-});
+const userSchema = mongoose.Schema(
+  {
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 userSchema.plugin(uniqueValidator);
 
